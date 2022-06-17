04c978b780074531a268c2cf7e6cefb152fd79e6
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2022-06-17 16:33:41 +0600

    ✨ Added classoptions for ticket

--- utils/options.py
+++ utils/options.py
@@ -8,4 +8,15 @@ class IdentificationTypeOptions(models.TextChoices):
 
     BIRTH_CERTIFICATE = 'BIRTH_CERTIFICATE', 'Birth Certificate'
     NATIONAL_ID = 'NID', 'NID'
+
+class ClassOptions(models.TextChoices):
+    ''' Class Options For Train '''	
+    SHOVAN = 'SHOVAN', 'Shovan'
+    S_CHAIR = 'S_CHAIR', 'Shovan Chair'
+    SNIGDHA = 'SNIGDHA', 'Snigdha'
+    AC_BERTH = 'AC_BERTH', 'AC BERTH'
+    AC_SEAT = 'AC_SEAT', 'AC Seat'
+    F_CHAIR = 'F_CHAIR', 'First Class Chair'
+    F_BERTH = 'F_BERTH', 'First Class Berth'
+    F_SEAT = 'F_SEAT', 'First Class Seat'
     
